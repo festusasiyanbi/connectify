5be85e7ad765dc82cac176bb2d3d41ead3979146
Festus <festusasiyanbi80@gmail.com>
2024-05-13 21:20:51 -0400

    entry point for my program

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from '../stacks/StackNavigator';
 import {ThemeProvider} from '../context/ThemeProvider';
+import Toast from 'react-native-toast-message';
 
 export default function App() {
   return (
